35f59c8b7358fc2141a47648665aae407ac00bd2
J-Kanami-PS <jazminpavon@fiuni.edu.py>
2025-11-15 17:19:15 -0300

    Tomcat

--- src/main/java/com/eureka/microservice/MicroserviceEurekaApplication.java
+++ src/main/java/com/eureka/microservice/MicroserviceEurekaApplication.java
@@ -2,14 +2,21 @@ package com.eureka.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MicroserviceEurekaApplication {
+public class MicroserviceEurekaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceEurekaApplication.class, args);
+        SpringApplication.run(MicroserviceEurekaApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Esto le dice a Tomcat cómo encontrar tu aplicación Spring
+        return application.sources(MicroserviceEurekaApplication.class);
+    }
 }
